3907819e8a3b051096c754344b17ebaabde8eb08
Jibin Rajan Varghese <email.jrv@gmail.com>
2019-04-27 01:38:59 -0500

    adding redundant nodes

--- interconnect.sh
+++ interconnect.sh
@@ -4,6 +4,9 @@ sshpass -p osboxes.org ssh osboxes@192.168.0.2 ping $OWN_IP -c 3 -q && echo -e "
 sshpass -p osboxes.org ssh osboxes@192.168.0.3 ping $OWN_IP -c 3 -q && echo -e "\e[32m192.168.0.3 OK\e[39m "
 sshpass -p osboxes.org ssh osboxes@192.168.0.4 ping $OWN_IP -c 3 -q && echo -e "\e[32m192.168.0.4 OK\e[39m "
 sshpass -p osboxes.org ssh osboxes@192.168.0.5 ping $OWN_IP -c 3 -q && echo -e "\e[32m192.168.0.5 OK\e[39m "
+sshpass -p osboxes.org ssh osboxes@192.168.0.6 ping $OWN_IP -c 3 -q && echo -e "\e[32m192.168.0.6 OK\e[39m "
+sshpass -p osboxes.org ssh osboxes@192.168.0.7 ping $OWN_IP -c 3 -q && echo -e "\e[32m192.168.0.7 OK\e[39m "
+
 echo $OWN_IP ' interconnected'
 
 
